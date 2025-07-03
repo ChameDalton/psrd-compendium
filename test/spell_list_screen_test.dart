3c05795b900781b293a946b919837eed5dc141c2
Chame Dalton <chame.dalton@gmail.com>
2025-07-03 02:15:44 -0600

    fixes after adding addl sql calls for details screen pt2

--- test/spell_list_screen_test.dart
+++ test/spell_list_screen_test.dart
@@ -5,16 +5,14 @@ import 'package:pathfinder_athenaeum/services/database_helper.dart';
 import 'package:pathfinder_athenaeum/screens/spell_list_screen.dart';
 import 'package:pathfinder_athenaeum/screens/spell_details_screen.dart';
 
-// Create a MockBuildContext
-class MockBuildContext extends Mock implements BuildContext {}
+// Mock DatabaseHelper for testing
+class MockDatabaseHelper extends Mock implements DatabaseHelper {}
 
 void main() {
   late MockDatabaseHelper mockDbHelper;
-  late MockBuildContext mockContext;
 
   setUp(() {
     mockDbHelper = MockDatabaseHelper();
-    mockContext = MockBuildContext();
   });
 
   testWidgets('SpellListScreen displays spells and navigates to details', (WidgetTester tester) async {
@@ -23,25 +21,48 @@ void main() {
       {'section_id': '2', 'name': 'Fireball', 'type': 'spell', 'parent_id': 2},
     ];
 
-    when(mockDbHelper.getSections(mockContext, 'spell')).thenAnswer((_) async => mockSpells);
+    // Create a context key to capture BuildContext
+    final contextKey = GlobalKey();
+
+    // Mock the getSections method to return a Future with mockSpells
+    when(mockDbHelper.getSections(any, 'spell')).thenAnswer((invocation) async {
+      final context = invocation.positionalArguments[0] as BuildContext;
+      if (context == contextKey.currentContext) {
+        return mockSpells;
+      }
+      throw Exception('Invalid BuildContext');
+    });
 
     await tester.pumpWidget(
       MaterialApp(
-        home: SpellListScreen(dbHelper: mockDbHelper),
+        home: Builder(
+          builder: (BuildContext context) {
+            // Assign context to contextKey
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              contextKey.currentContext;
+            });
+            return SpellListScreen(dbHelper: mockDbHelper);
+          },
+        ),
       ),
     );
 
+    // Wait for the FutureBuilder to complete
     await tester.pumpAndSettle();
 
+    // Verify the UI displays the expected spell names
     expect(find.text('Spells'), findsOneWidget);
     expect(find.text('Magic Missile'), findsOneWidget);
     expect(find.text('Fireball'), findsOneWidget);
 
+    // Tap on 'Magic Missile' to trigger navigation
     await tester.tap(find.text('Magic Missile'));
     await tester.pumpAndSettle();
 
-    verify(mockDbHelper.getSections(mockContext, 'spell')).called(1);
+    // Verify that getSections was called with the correct context
+    verify(mockDbHelper.getSections(contextKey.currentContext, 'spell')).called(1);
 
+    // Verify navigation to SpellDetailsScreen with correct parameters
     expect(
       find.byWidgetPredicate(
         (widget) =>
@@ -53,6 +74,3 @@ void main() {
     );
   });
 }
-
-// Mock DatabaseHelper for testing
-class MockDatabaseHelper extends Mock implements DatabaseHelper {}
